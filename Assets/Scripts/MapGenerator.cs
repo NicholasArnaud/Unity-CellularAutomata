7609f2463c7a9553e1db2c40d77702cfb6cf63ca
Nicholas Arnaud <cobrahead09@gmail.com>
2017-10-20 01:17:02 -0500

    InitialCommit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public string seed;
+    public bool useRandomSeed;
+
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    int[,] map;
+
+    private void Start()
+    {
+        GenerateMap();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
+        for (int i = 0; i < 5; i++)
+        {
+            SmoothMap();
+        }
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+        System.Random psuedoRandom = new System.Random(seed.GetHashCode());
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    map[x, y] = 1;
+                else
+                    map[x, y] = (psuedoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+            }
+        }
+    }
+
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+
+                if (neighbourWallTiles > 4)
+                    map[x, y] = 1;
+                else if (neighbourWallTiles < 4)
+                    map[x, y] = 0;
+
+            }
+        }
+    }
+
+    int GetSurroundingWallCount(int gridx, int gridy)
+    {
+        int wallCount = 0;
+        for (int neighX = gridx - 1; neighX <= gridx + 1; neighX++)
+        {
+            for (int neighY = gridy - 1; neighY <= gridy + 1; neighY++)
+            {
+                if (neighX >= 0 && neighX < width && neighY >= 0 && neighY < height)
+                {
+                    if (neighX != gridx || neighY != gridy)
+                    {
+                        wallCount += map[neighX, neighY];
+                    }
+                }
+                else
+                    wallCount++;
+
+            }
+        }
+        return wallCount;
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+
+    }
+}
