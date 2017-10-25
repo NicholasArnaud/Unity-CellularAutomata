4a17088a29b29b59b48d438fdfdf3830562ddbcc
Nicholas Arnaud <cobrahead09@gmail.com>
2017-10-24 23:08:59 -0500

    Finish Unity Tutorial

---
+++ Assets/Scripts/Player2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+    Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
